4a54c0c0d162e8c328a82a95c580a9a84e7469bc
Pradeep <spradeepreddy574@gmail.com>
2021-02-16 20:47:21 +0530

    Added Product List Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,11 @@ import {Component} from '@angular/core';
   selector : 'pm-root',
   template : `
   <div>
-  <h1>
-    Welcome to {{pageTitle}}
-  </h1>
-  ... Stater Files..
+  <h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
 </div>
   `
 })
 export class AppComponent{
   pageTitle : string = "Acme Product Management";
-}
+} 
